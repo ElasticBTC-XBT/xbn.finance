c5175dd557c91a305b86076b3e2bf2b3256d1155
elasticbitcoin-xbt <74126676+elasticbitcoin-xbt@users.noreply.github.com>
2020-11-08 12:01:56 +0700

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <fragment>
+        <c-hero-split image-fill class="illustration-section-01" />
+        <c-features-tiles />
+        <c-features-split invert-mobile image-fill class="illustration-section-05" />
+        <c-pricing pricing-switcher class="illustration-section-03" />
+        <c-cta top-divider bottom-divider split />
+    </fragment>
+</template>
+
+<script>
+// import layout
+import CLayout from '@/layouts/LayoutDefault.vue'
+// import sections
+import CHeroSplit from '@/components/sections/HeroSplit.vue'
+import CFeaturesSplit from '@/components/sections/FeaturesSplit.vue'
+import CFeaturesTiles from '@/components/sections/FeaturesTiles.vue'
+import CPricing from '@/components/sections/Pricing.vue'
+import CCta from '@/components/sections/Cta.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    CHeroSplit,
+    CFeaturesSplit,
+    CFeaturesTiles,
+    CPricing,
+    CCta
+  },
+  created() {
+    this.$emit('update:layout', CLayout)
+  }
+}
+</script>
