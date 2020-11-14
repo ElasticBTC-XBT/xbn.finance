698c68d70eb7bf3c18975b8ad7fe3bc7c0ffff2f
Tran Dinh Khang <khangtd.09@gmail.com>
2020-11-14 10:00:01 +0700

    fix: fix scss loading and standardize initial source

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const history = require('connect-history-api-fallback');
 
 let app = express();
 app.use(history());
-app.use(serveStatic(__dirname + '/dist'));
+app.use(serveStatic(__dirname + '/docs'));
 
 const port = process.env.PORT || 5000;
 app.listen(port);
+console.log(`Server are running at: http://localhost:${port}`)
