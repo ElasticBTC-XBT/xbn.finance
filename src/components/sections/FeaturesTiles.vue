7e173b88b0d5c0537a958cc9d05cd08ebc020834
Alan The Achiever <alantheachiever@elasticbitcoin.org>
2020-11-23 00:42:56 +0700

    fix: change icon

--- src/components/sections/FeaturesTiles.vue
+++ src/components/sections/FeaturesTiles.vue
@@ -65,27 +65,27 @@ export default {
       },
       sectionDataUpper: [
         {
-          image: require('@/assets/images/feature-tile-icon-06.svg'),
+          image: require('@/assets/images/feature-tile-icon-01.svg'),
           title: 'A new DeFi protocol',
           description: 'XBT is a DeFi protocol with an elastic supply model. This means that the total supply of XBT is constantly changing.'
         },
         {
-          image: require('@/assets/images/feature-tile-icon-06.svg'),
+          image: require('@/assets/images/feature-tile-icon-02.svg'),
           title: 'Randomized rebasing',
           description: 'XBT reaches a supply-price equilibrium with its randomized rebasing. As a result, the volatility is in the token supply, not the price.'
         },
         {
-          image: require('@/assets/images/feature-tile-icon-06.svg'),
+          image: require('@/assets/images/feature-tile-icon-03.svg'),
           title: 'Price target of $1 BTC',
           description: 'is designed to maintain its base price target of $1 BTC with a CPI inflation adjustment.'
         },
         {
-          image: require('@/assets/images/feature-tile-icon-06.svg'),
+          image: require('@/assets/images/feature-tile-icon-04.svg'),
           title: 'Holdings rise with market cap',
           description: 'Holders of the coin own a percentage of the network. Their holdings rise in value as market cap rises.'
         },
         {
-          image: require('@/assets/images/feature-tile-icon-06.svg'),
+          image: require('@/assets/images/feature-tile-icon-05.svg'),
           title: 'A rebase event',
           description: 'Rebase is the event of adjusting the balance of all wallets based on the current price of the coin related to the target price.'
         },
