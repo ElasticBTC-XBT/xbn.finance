887cb25f81b9817cb9e0a3f76f1a980067188130
Nam Tran <namth3979@gmail.com>
2020-12-27 01:51:23 +0700

    change_multilanguage_ver_2

--- src/App.vue
+++ src/App.vue
@@ -16,31 +16,9 @@
                 layout: 'div'
             }
         },
-        methods: {
-            getLangFromURL() {
-                let url_string = window.location.href
-                let url = new URL(url_string);
-                let lang = url.searchParams.get("lang");
-                return lang
-            },
-            setLang(lang) {
-                this.$i18n.locale = lang;
-                localStorage.setItem('lang_elastic', lang)
-            }
-        },
+        methods: {},
         mounted() {
             document.body.classList.add('is-loaded')
-
-            if (this.getLangFromURL()) {
-                this.setLang(this.getLangFromURL())
-            } else {
-                let lang_local_storage = localStorage.getItem('lang_elastic')
-                if (!lang_local_storage) {
-                    this.setLang('en')
-                } else {
-                    this.setLang(lang_local_storage)
-                }
-            }
         }
     }
 </script>
