e2ef987a25734dadb81311e45fa70ba4fb2fb9ea
namth3979 <deakil@moonrat.finance>
2021-04-13 15:54:37 +0700

    test call api twitter

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
+var cors = require('cors');
 
 let app = express();
 app.use(history());
 app.use(serveStatic(__dirname + '/dist'));
 
+app.use(cors());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Expose-Headers", "X-My-Custom-Header, X-Another-Custom-Header");
-    next(); // make sure we go to the next routes and don't stop here
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization");
+    res.header("Content-Type", "application/json");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
 });
 
 const port = process.env.PORT || 5000;
