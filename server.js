d63d2a6e44341cc6f5f3828e8861ac589ad5ad99
namth3979 <deakil@moonrat.finance>
2021-04-13 15:50:59 +0700

    test call api twitter

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(serveStatic(__dirname + '/dist'));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Expose-Headers", "X-My-Custom-Header, X-Another-Custom-Header");
     next(); // make sure we go to the next routes and don't stop here
 });
 
