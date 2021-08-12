ba8aaa5763b00efd64d0ffb6646e0a4534a8e199
Adsigma <hong@cloudjetkpi.com>
2021-08-12 11:09:06 +0700

    CTA

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -8,7 +8,7 @@
                 </div>
                 <div class="footer-bottom space-between text-xxs invert-order-desktop">
                     <c-footer-nav />
-                    <div class="footer-copyright">&copy; 2020 XBN, all rights reserved</div>
+                    <div class="footer-copyright">&copy; 2021 XBN</div>
                 </div>
             </div>
         </div>
